62ab72c82b52a87030b36c67e9d4f8b1efee26d7
Yashkumar <jainy6214@gmail.com>
2021-12-12 18:18:49 +0530

    written system calls read,write

--- system.c
+++ system.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<unistd.h>
-#include<stdio.h> 
 #include<fcntl.h> 
 #include<errno.h> 
 #include<stdlib.h>
@@ -22,11 +21,14 @@ int main()
     printf("Number of bytes written in file is %d\n",size);
     close(fd);
     //Write System Call
-    fd=open("text1.txt",O_WRONLY | O_CREAT | O_APPEND);
+    fd=open("text1.txt",O_WRONLY | O_EXCL | O_TRUNC);
     size=write(fd,"Hello World to System Calls\n",strlen("Hello World to System Calls\n"));
     printf("fd is :%d\n",fd);
     printf("the number of bits written is %d\n",size);
     //Deleting using unlink
     fd = open("text.txt", O_RDONLY | O_CREAT);
     unlink("text.txt");
+    //lseek system call
+    
+
 }
