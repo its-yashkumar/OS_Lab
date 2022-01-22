b16fc95497d088322eec2004f904e4416d81088e
Yashkumar <jainy6214@gmail.com>
2022-01-22 14:08:43 +0530

    cebugged it

--- exec1.c
+++ exec1.c
@@ -6,8 +6,6 @@ int main (int argc,char *argv[] )
 {
 printf("process id of Exec.c= %d\n",getpid());
 char *args[]={"Hi","Welcome","To","Lab",NULL};
-// printf("The string is %s",*argv[argc]);
-gets(*argv[argc]);
 execv("./Exec2",args);
 printf("Back to this program Exec\n");
 return 0;
